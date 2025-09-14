719efe248a5e01790284f46166c67d73d2266144
Beldumite <azkaarrofiq12@gmail.com>
2025-09-15 00:34:36 +0700

    basic wasd

---
+++ public/characters/Walk-Anim.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Walk-Anim" tilewidth="24" tileheight="48" tilecount="32" columns="4">
+ <image source="Walk-Anim.png" width="96" height="384"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,17 +1,98 @@
+// import * as Phaser from 'phaser';
+// import { SCENE_KEYS } from './scene-keys';
+// import { ASSET_KEYS } from '../common/assets';
+
+// export class GameScene extends Phaser.Scene {
+//   constructor() {
+//     super({
+//       key: SCENE_KEYS.GAME_SCENE,
+//     });
+//   }
+
+//   public create(): void {
+//     this.add
+//       .text(this.scale.width / 2, this.scale.height / 2, 'Game Scene', { fontFamily: ASSET_KEYS.FONT_PRESS_START_2P })
+//       .setOrigin(0.5);
+//   }
+// }
+
+
 import * as Phaser from 'phaser';
 import { SCENE_KEYS } from './scene-keys';
-import { ASSET_KEYS } from '../common/assets';
 
 export class GameScene extends Phaser.Scene {
+  private player!: Phaser.Physics.Arcade.Sprite;
+  private keys!: { W: Phaser.Input.Keyboard.Key; A: Phaser.Input.Keyboard.Key; S: Phaser.Input.Keyboard.Key; D: Phaser.Input.Keyboard.Key; };
+
   constructor() {
-    super({
-      key: SCENE_KEYS.GAME_SCENE,
+    super({ key: SCENE_KEYS.GAME_SCENE });
+  }
+
+  public preload(): void {
+    this.load.spritesheet('victini', 'characters/Walk-Anim.png', {
+      frameWidth: 24,
+      frameHeight: 48,
     });
   }
 
   public create(): void {
-    this.add
-      .text(this.scale.width / 2, this.scale.height / 2, 'Game Scene', { fontFamily: ASSET_KEYS.FONT_PRESS_START_2P })
-      .setOrigin(0.5);
+    this.player = this.physics.add.sprite(this.scale.width / 2, this.scale.height / 2, 'victini');
+    this.player.setCollideWorldBounds(true);
+
+    this.keys = this.input.keyboard.addKeys({
+      W: Phaser.Input.Keyboard.KeyCodes.W,
+      A: Phaser.Input.Keyboard.KeyCodes.A,
+      S: Phaser.Input.Keyboard.KeyCodes.S,
+      D: Phaser.Input.Keyboard.KeyCodes.D,
+    }) as any;
+
+    this.anims.create({
+      key: 'walk-down',
+      frames: this.anims.generateFrameNumbers('victini', { start: 0, end: 3 }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'walk-left',
+      frames: this.anims.generateFrameNumbers('victini', { start: 24, end: 26 }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'walk-right',
+      frames: this.anims.generateFrameNumbers('victini', { start: 8, end: 11 }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'walk-up',
+      frames: this.anims.generateFrameNumbers('victini', { start: 16, end: 19 }),
+      frameRate: 8,
+      repeat: -1,
+    });
+  }
+
+  public update(): void {
+    const speed = 150;
+    this.player.setVelocity(0);
+
+    if (this.keys.W.isDown) {
+      this.player.setVelocityY(-speed);
+      this.player.anims.play('walk-up', true);
+    } else if (this.keys.S.isDown) {
+      this.player.setVelocityY(speed);
+      this.player.anims.play('walk-down', true);
+    } else if (this.keys.A.isDown) {
+      this.player.setVelocityX(-speed);
+      this.player.anims.play('walk-left', true);
+    } else if (this.keys.D.isDown) {
+      this.player.setVelocityX(speed);
+      this.player.anims.play('walk-right', true);
+    } else {
+      this.player.anims.stop();
+    }
   }
 }
